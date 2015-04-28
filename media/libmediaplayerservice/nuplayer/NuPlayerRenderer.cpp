8471ec0601d48830e0ad4cb781a3e775ed413b72
Preetam Singh Ranawat <apranawat@codeaurora.org>
2015-04-28 20:45:55 +0530

    NuPlayer: Fix incorrect time update for pause followed by seek, resume
     -During pause, seek and resume sequence progress bar moves
      back to paused time stamp and then come back to actual seek
      position on resuming.
     -On resuming NuPlayer renderer gives last pause position if
      getcurrent position is called before state is set to running.
     -reset the pause position during flush to fix the above scenario.
    
    Change-Id: I75e0acab0bff9851cd223645c461d6d8bb0633c1
    
    Conflicts:
    	media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp

--- media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
@@ -1155,7 +1155,10 @@ void NuPlayer::Renderer::onFlush(const sp<AMessage> &msg) {
     {
          Mutex::Autolock autoLock(mLock);
          syncQueuesDone_l();
-         setPauseStartedTimeRealUs(-1);
+         if (!(offloadingAudio() && mPaused)) {
+             setPauseStartedTimeRealUs(-1);
+         }
+         mPausePositionMediaTimeUs = -1;
          setAnchorTime(-1, -1);
     }
 
